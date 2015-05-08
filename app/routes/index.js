c0f4293e67faab6a066742d0eb7a72f745ecc099
James Futhey <futhey@gmail.com>
2015-05-07 23:11:27 -0700

    Commentify

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
+    // Fetches all post records from the store
     return this.store.findAll('post');
   },
   actions: {
     createPost() {
+      // Creates a reference to our index, so we can
+      // capture the values in the #NewPost form
       let index = this.controllerFor('index');
 
+      // Creates a new post record in ember-data's data-store
       let post = this.store.createRecord('post', {
         title: index.get('title'),
         text: index.get('text'),
@@ -15,8 +19,10 @@ export default Ember.Route.extend({
         createdDate: new Date()
       });
 
+      // Save our new record
       post.save();
 
+      // Reset the form so that each field is blank
       Ember.$("#NewPost")[0].reset();
     }
   }
